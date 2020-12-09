818a4c85d93652a44ce2ea7d2aabb333808786fa
Propioception <github email address>
2020-12-08 21:35:58 -0500

    edit reqire lines to enviroment

--- lib/cocktail4u.rb
+++
@@ -1,6 +0,0 @@
-require "cocktail4u/version"
-
-module Cocktail4u
-  class Error < StandardError; end
-  # Your code goes here...
-end
---
+++ lib/enviroment.rb
@@ -0,0 +1,16 @@
+require 'pry'
+require "net/http"
+require "open-uri"
+require_relative "./cocktail4u/version"
+require_relative "./api"
+require_relative "./cli"
+require_relative "./cocktail"
+require_relative "./ingredient"
+
+
+
+
+module Cocktail4u
+  class Error < StandardError; end
+  # Your code goes here...
+end
