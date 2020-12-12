f902635fa2801f4d828710129a698c2929cf50ab
Propioception <github email address>
2020-12-11 20:03:27 -0500

    create option for user inside use_selection method

--- lib/api.rb
+++ lib/api.rb
@@ -1,4 +1,4 @@
-class API 
+class API  
 
     def self.find_cocktail_by_ingredient(ingredient)
         url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=#{ingredient}"
@@ -8,7 +8,6 @@ class API
         return nil if json["drinks"] ==  nil 
         json["drinks"].each do  |drink|
             hash = {:id => drink["idDrink"], :name => drink["strDrink"], :category => drink["strCategory"], :type => drink["strAlcoholic"], :instructions => drink["strInstructions"]}
-            # binding.pry
             cocktail = Cocktail.find_or_create_by_name(hash)
             ingredients = [drink["strIngredient1"], drink["strIngredient2"], drink["strIngredient3"], drink["strIngredient4"], drink["strIngredient5"], drink["strIngredient6"], drink["strIngredient7"], drink["strIngredient8"], drink["strIngredient9"], drink["strIngredient10"]]
             ingredients.compact.each do |i|
@@ -17,7 +16,6 @@ class API
             #   object.cocktails << cocktail
             end
         end
-        # binding.pry 
     end 
 
 end 
--- lib/cli.rb
+++ lib/cli.rb
@@ -15,12 +15,12 @@ class CLI
     end 
 
     def ingredient_selection
-        puts "Before you get all Alchee on us, type an ingredient you want to get Alchee with..."
+        puts "Please type an ingredient you want to get to get Alchee with..."
         @input = gets.strip.downcase
         @maybe_nil = API.find_cocktail_by_ingredient(@input)
         if !@maybe_nil      
-            puts " asdsda"  
-        ingredient_selection
+            puts " Real Alchee's dont use that ingredient, please try another one."  
+            ingredient_selection
         end 
     end 
 
@@ -29,24 +29,44 @@ class CLI
         @cocktails.each.with_index(1) do |cocktail, i| 
             puts "#{i}. #{cocktail.name}"
         end 
+        puts "If you want to exit type exit, if you want to select a cocktail enter its number or if you would like to select a different ingredient type ingredient"
+        
     end
     
     def user_selection
-        @cocktail_input = gets.strip.to_i
-        if @cocktail_input.between?(1, @cocktails.length)
-            @cocktail = @cocktails[@cocktail_input-1]
-        display_cocktail
+        @cocktail_input = gets.strip.downcase 
+        if @cocktail_input == "exit"
+            puts "Goodbye!"
+        elsif @cocktail_input == "ingredient" 
+            run  
+        elsif @cocktail_input.to_i.between?(1, @cocktails.length)
+            @cocktail = @cocktails[@cocktail_input.to_i - 1]
+            display_cocktail
         else 
-            puts "wrong input please type a number from 1 to #{@cocktails.length}"
-        user_selection
+            puts "Someone is wavy =) Wrong input please type a number from 1 to #{@cocktails.length}"
+            user_selection
         end 
     end 
 
 
     def display_cocktail
-        puts @cocktail.name 
+        puts @cocktail.name
+        @cocktail.ingredients.each do |ingredient| 
+            puts ingredient.name
+        end  
+        puts @cocktail.instructions
+        @cocktail_input = gets.strip.downcase 
+        # if @cocktail_input == "exit"
+        #     puts "Goodbye!"
+        # puts "if you want to go back type back otherwise type ingredient to see a new collection of cocktails using your ingredient"
 
-    end 
+    end
+
+
+
+    
+    
+
 
             
 
