921794602972d653d6c2e0b38a2ab71c240222a4
Propioception <github email address>
2020-12-24 11:55:31 -0500

    add find_by_category

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,27 +1,30 @@
-class CLI 
+class CLI  
+    
     def run
         welcome
     end 
     
     def welcome
-        puts "******************"
-        puts "******************"
-        puts "Welcome to Alchee!".blue
-        puts "******************"
-        puts "******************"
+        puts "******************".white
+        puts "******************".white
+        puts "Welcome to Alchee!".red
+        puts "******************".white
+        puts "******************".white
         ingredient_selection
     end 
 
     def ingredient_selection
-        puts "Please type an ingredient you want to get to get Alchee with...otherwise TYPE exit"
+        puts "Please type an ingredient you want to get to get Alchee with...otherwise TYPE exit".red 
         @input = gets.strip.downcase
-        @maybe_nil = API.find_cocktail_by_ingredient(@input)
+        @maybe_nil= Ingredient.find_by_name(@input) ? Ingredient.find_by_name(@input) : API.find_cocktail_by_ingredient(@input)
         if @input != "exit"          
             if !@maybe_nil      
                 puts "Real Alchee's dont use that ingredient, please try another one."  
                 ingredient_selection
             end 
             display_cocktails
+        else
+            goodbye
         end 
     end 
 
@@ -29,25 +32,30 @@ class CLI
         @cocktails = Cocktail.all
         @cocktails.each.with_index(1) do |cocktail, i| 
             puts "#{i}. #{cocktail.name}"
-        end 
+        end
+        # binding.pry 
         puts "#####################################################################################################################################################"
-        puts "If you want to EXIT type exit, if you want to SELECT a cocktail enter its number or if you would like to select a DIFFERENT ingredient type ingredient"
+        puts "If you want to EXIT type exit, if you want to SELECT a cocktail enter its number or if you would like to select a DIFFERENT ingredient type ingredient".red
         user_selection
     end
    
     def user_selection
         @cocktail_input = gets.strip.downcase 
         if @cocktail_input == "exit"
-            puts "Goodbye!"
+            goodbye
         elsif @cocktail_input == "ingredient" 
             run  
         elsif @cocktail_input.to_i.between?(1, @cocktails.length)
             @cocktail = @cocktails[@cocktail_input.to_i - 1]
             display_cocktail
         else 
-            puts "WRONG input please type a NUMBER from 1 to #{@cocktails.length}"
+            puts "WRONG input please type a NUMBER from 1 to #{@cocktails.length}".red
             user_selection
         end 
+    end 
+
+    def goodbye
+        puts "Goodbye!".red
     end 
 
     def display_cocktail
@@ -56,12 +64,16 @@ class CLI
             puts ingredient.name
         end  
         puts @cocktail.instructions
+        puts @cocktail.category 
+        # puts @cocktail.category 
+        # binding.pry 
         puts "####################################"
-        puts "To see PREVIOUS COCKTAIL list type back, otherwise TYPE exit"
+        puts "To see PREVIOUS COCKTAIL list type back, otherwise TYPE exit".red
         @cocktail_input = gets.strip.downcase 
         if @cocktail_input == "back"
             display_cocktails
-           
         end
     end 
 end 
+            
+           
--- lib/cocktail.rb
+++ lib/cocktail.rb
@@ -1,4 +1,4 @@
-class Cocktail 
+class Cocktail  
 
     @@all =[]
 
@@ -11,18 +11,25 @@ class Cocktail
     def initialize(hash)
         hash.each do |key, value|
             self.class.attr_accessor key 
-            self.send("#{key}=", value)
+            self.public_send("#{key}=", value)
         end 
         self.ingredients = []
         self.class.all << self
+        # binding.pry 
     end 
+        
+    def self.find_by_category(category_input)
+        self.all.find_all {|cocktail| cocktail.category == category_input}
+    end
 
     def self.find_by_name(name)
-        self.all.find {|ingredient_object| ingredient_object.name == name} 
+        self.all.index{|ingredient_object| ingredient_object.name == name} 
     end 
 
     def self.find_or_create_by_name(hash)
         self.find_by_name(hash[:name]) || self.new(hash)
     end 
+
+
 end 
 
